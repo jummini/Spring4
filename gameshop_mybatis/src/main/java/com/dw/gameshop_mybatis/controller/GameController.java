8627cf3102ed1d953759c2d1f04305e142780f7d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-08-04 17:30:29 +0900

    Update GameController.java

--- gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/controller/GameController.java
+++ gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/controller/GameController.java
@@ -4,6 +4,8 @@ import com.dw.gameshop_mybatis.dto.GameDTO;
 import com.dw.gameshop_mybatis.model.Game;
 import com.dw.gameshop_mybatis.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,16 @@ public class GameController {
     GameService gameService;
 
     @GetMapping("/all")
-    public List<GameDTO> getAllGames() {
-        return gameService.getAllGames();
+    public ResponseEntity<List<GameDTO>> getAllGames() {
+        return new ResponseEntity<>(
+                gameService.getAllGames(),
+                HttpStatus.OK);
     }
 
     @GetMapping("/id/{id}")
-    public GameDTO getGameById(@PathVariable long id) {
-        return gameService.getGameById(id);
+    public ResponseEntity<GameDTO> getGameById(@PathVariable long id) {
+        return new ResponseEntity<>(
+                gameService.getGameById(id),
+                HttpStatus.OK);
     }
 }
