57398fcaeff3646851f99a503c48ab998f30b970
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-08-06 09:41:38 +0900

    Update BoardMapper.java

--- gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/mapper/BoardMapper.java
+++ gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/mapper/BoardMapper.java
@@ -1,7 +1,16 @@
 package com.dw.gameshop_mybatis.mapper;
 
+import com.dw.gameshop_mybatis.model.Board;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
+    Board getBoardById(@Param("id") long id);
+    List<Board> getAllBoards();
+    int saveBoard(@Param("board") Board board);
+    int updateBoard(@Param("board") Board board);
+    int deleteBoard(@Param("id") long id);
 }
