ad9fc3df6c325bbe04de6cff9022f859d783f8de
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-08-08 15:45:51 +0900

    Update UserDTO.java

--- gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/dto/UserDTO.java
+++ gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.dw.gameshop_mybatis.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -15,6 +16,8 @@ public class UserDTO {
     @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
     private String userName;
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+         message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
