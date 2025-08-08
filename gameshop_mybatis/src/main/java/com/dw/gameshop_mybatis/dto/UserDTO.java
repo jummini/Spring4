bde1a43420a492efacad2d842907570bc414ab5a
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-08-08 15:01:51 +0900

    Update UserDTO.java

--- gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/dto/UserDTO.java
+++ gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.dw.gameshop_mybatis.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -8,9 +11,15 @@ import lombok.*;
 @Setter
 @ToString
 public class UserDTO {
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
     private String userName;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String realName;
     private String role;
 }
